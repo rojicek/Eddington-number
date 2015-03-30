cba23368b9a9e084fc33c7d7cee38d8a1fafbdcd
Jiri Rojicek <jiri@rojicek.cz>
2015-03-30 20:44:39 +0200

    rewritten using EDN function
    EDN calc moved to function

--- index.php
+++ index.php
@@ -14,6 +14,10 @@
 <body>
 <?php
 
+
+include 'enfce.php';
+
+
 session_start();
 $mileCoef = 1.609344;
 $token =  $_SESSION['stravatoken'];
@@ -28,17 +32,12 @@ if (is_null($token))
  {//login
 //jsem prihlaseny, muzu nacist aktivity 
  
-
- //echo "main token: ". $_GET['ses'] . " <br>";
  
  $url = "https://www.strava.com/api/v3/athlete?access_token=" . $token;
  $result = file_get_contents($url);
  $athlete = json_decode ($result, true);
 
-// echo $result;
-// echo "<p>";
-// echo "id: " . $athlete["id"];
-// echo "<p>";
+
  
  //get rides
  
@@ -56,9 +55,12 @@ $predrokem = strtotime(date("Y-m-d", strtotime(date("Y-m-d") . " - 1 year")));
  if (empty($rides))
  {
   break; //zadne dalsi jizdy
-}
-  
-  //$distances=array();
+ }
+ 
+
+ //create array fof lifes (all live / last year)
+ //metric and statute -> 4 arrays
+ //can be more pages (max 200 rides per page by Strava)
   $ix = 0;
   $ixLife = 0;
  foreach ($rides as $oneride)
@@ -69,16 +71,16 @@ $predrokem = strtotime(date("Y-m-d", strtotime(date("Y-m-d") . " - 1 year")));
        //last year
        if (($rideDate >= $predrokem )   && (strtolower($oneride["type"]) == 'ride'))
        {
-        $distances[$ix] =  $oneride["distance"];   
-        $distancesStatute[$ix] = $distances[$ix] / $mileCoef;     
+        $distances[$ix] =  floor($oneride["distance"]/1000);   
+        $distancesStatute[$ix] = floor($oneride["distance"] / $mileCoef / 1000);     
         $ix = $ix + 1;
       } 
       
       //all time
       if ((strtolower($oneride["type"]) == 'ride'))
        {                 
-        $distancesLife[$ixLife] =  $oneride["distance"];
-        $distancesLifeStatute[$ixLife] = $distancesLife[$ixLife] / $mileCoef;        
+        $distancesLife[$ixLife] =  floor($oneride["distance"]/1000);
+        $distancesLifeStatute[$ixLife] = floor($oneride["distance"] / $mileCoef / 1000);       
         $ixLife = $ixLife + 1;
       } 
       
@@ -87,95 +89,14 @@ $predrokem = strtotime(date("Y-m-d", strtotime(date("Y-m-d") . " - 1 year")));
   $page=$page + 1;
 }
  
-  $pocetJizd = $ix;
-  $pocetJizdLife = $ixLife;
-  
-  $edn = 0;
-  $ednLife = 0;
-  
  
- $razeni = sort($distances);
- $razeniStatute = sort($distancesStatute);
- 
- 
- $razeniLife = sort($distancesLife);
- $razeniLifeStatute = sort($distancesLifeStatute);
- 
- 
- //debug
- //testovani algoritmu na rucnich datech
- /*
- $distances = array (1000,2000,4458,5548,10555,11523,12545,44545,23454,33414,23447,22556,1556,12458,2545,52454,7542,13545,4545,85245,12254,8876,13547, 44857, 55887, 44587);
- $pocetJizd = sizeof ($distances);
- $razeni = sort($distances);
- */
- /*
- for ($ix = 0; $ix <$pocetJizd; $ix++)
- {
-  echo "trip " . ($ix+1) . " - (" .($pocetJizd - $ix) . ") dist: " . floor ($distances[$ix] / 1000) . "<br>";
- }
- */
- //debug
+  $edn  = GetEN($distances);
+  $ednStatute  = GetEN($distancesStatute);
+  $ednLife  = GetEN($distancesLife);
+  $ednLifeStatute  = GetEN($distancesLifeStatute);
  
  
  
- //  echo  "##########################################################<br>";
- //yearly metric
-   for ($ix = 0; $ix <$pocetJizd; $ix++)
- {
-      $distances[$ix] = floor ($distances[$ix] / 1000);
-      $stejnychAdelsich = ($pocetJizd - $ix);
-      
-      if ($stejnychAdelsich>=$distances[$ix])         
-         $edn = $distances[$ix];
-        else
-         break;      
- }
-
-  //yearly statute - asi by to slo spojit, protoze metricke EN bude mensi
-   for ($ix = 0; $ix <$pocetJizd; $ix++)
- {
-      $distancesStatute[$ix] = floor ($distancesStatute[$ix] / 1000);
-      $stejnychAdelsichStatute = ($pocetJizd - $ix);
-      
-      if ($stejnychAdelsichStatute>=$distancesStatute[$ix])
-         $ednStatute = $distancesStatute[$ix];
-        else
-         break;      
- }
-
-
-// echo "pocet jizd life:" . $pocetJizdLife . "<br>"; 
-
- //life time
-    for ($ixLife = 0; $ixLife <$pocetJizdLife; $ixLife++)
- {
-      $distancesLife[$ixLife] = floor ($distancesLife[$ixLife] / 1000);
-      $stejnychAdelsichLife = ($pocetJizdLife - $ixLife);
-      
-     // echo "ride #". $ixLife . " left=" .   $stejnychAdelsichLife . " delka=" . $distancesLife[$ixLife] . "<br>" ;
-      
-      if ($stejnychAdelsichLife>=$distancesLife[$ixLife])
-         $ednLife = $distancesLife[$ixLife];
-        else
-         break;      
-         
- }
- 
-  //life time  statute
-    for ($ixLife = 0; $ixLife <$pocetJizdLife; $ixLife++)
- {
-      $distancesLifeStatute[$ixLife] = floor ($distancesLifeStatute[$ixLife] / 1000);
-      
-      $stejnychAdelsichLifeStatute = ($pocetJizdLife - $ixLife);
-         
-      
-      if ($stejnychAdelsichLifeStatute>=$distancesLifeStatute[$ixLife])
-         $ednLifeStatute = $distancesLifeStatute[$ixLife];
-        else
-         break;      
-         
- }
  
  
  echo  "Your last year metric <a href=\"http://triathlete-europe.competitor.com/2011/04/18/measuring-bike-miles-eddington-number\">Eddington number</a> is <b>". $edn . "</b>. <br>";
